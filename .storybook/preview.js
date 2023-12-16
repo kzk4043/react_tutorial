6c5dd08d4a9bc04b1051249f2175f1abc5b02804
kzk4043 <okazakishoma@team-lab.com>
2023-12-16 09:11:38 +0900

    fix: stylesの一括import

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+import "../src/styles.css";
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
--- src/stories/App/Board.stories.js
+++ src/stories/App/Board.stories.js
@@ -1,6 +1,4 @@
 import { Board } from "../../App";
-// styles.cssを読み込む
-import "../../styles.css";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {

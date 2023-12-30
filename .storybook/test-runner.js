83bca4b636d90c759ccca635dad26ded024a388f
kzk4043 <okazakishoma@team-lab.com>
2023-12-30 21:29:48 +0900

    feat: add vrt

--- .storybook/test-runner.js
+++ .storybook/test-runner.js
@@ -1,9 +1,24 @@
+// import { waitForPageReady } from "@storybook/test-runner";
+
+import { toMatchImageSnapshot } from "jest-image-snapshot";
+
+const customSnapshotsDir = `${process.cwd()}/__image_snapshots__`;
+
 const config = {
+  setup() {
+    expect.extend({ toMatchImageSnapshot });
+  },
   async postVisit(page, context) {
-    // the #storybook-root element wraps the story. In Storybook 6.x, the selector is #root
-    const elementHandler = await page.$("#storybook-root");
-    const innerHTML = await elementHandler.innerHTML();
-    expect(innerHTML).toMatchSnapshot();
+    // Waits for the page to be ready before taking a screenshot to ensure consistent results
+    // [Q] 公式では以下記載されているが、これを記載すると、タイムアウトする…検索してもめぼしい情報がない
+    // await waitForPageReady(page);
+
+    // To capture a screenshot for for different browsers, add page.context().browser().browserType().name() to get the browser name to prefix the file name
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot({
+      customSnapshotsDir,
+      customSnapshotIdentifier: context.id,
+    });
   },
 };
 
